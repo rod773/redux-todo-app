05723fcf80df010fb8552320fe529240db34e335
rod773 <104315743+rod773@users.noreply.github.com>
2024-02-11 21:00:58 +0100

    update

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,8 +1,18 @@
 import React, { useState } from "react";
 import { BsPlus } from "react-icons/bs";
+import { useDispatch } from "react-redux";
+import { addTodo } from "./../redux/actions";
 
 const Todo = () => {
+  const dispatch = useDispatch();
+
+  const handleAddTodo = (text) => {
+    dispatch(addTodo(text));
+  };
+
   const [newTodoText, setMewTodoText] = useState("");
+
+  const handleAddTodoClick = function () {};
 
   return (
     <div className="max-w-4xl mx-auto sm:mt-8 p-4 bg-gray-100 rounded">
@@ -19,7 +29,10 @@ const Todo = () => {
           placeholder="Add Todo"
           className="flex-grow p-2 border-b-2 border-gray-300 focus:outline-none focus:border-blue-500"
         />
-        <button className="ml-4 p-2 bg-blue-400 text-white rounded  hover:bg-blue-600">
+        <button
+          className="ml-4 p-2 bg-blue-400 text-white rounded  hover:bg-blue-600"
+          onClick={handleAddTodoClick}
+        >
           <BsPlus />
         </button>
       </div>
