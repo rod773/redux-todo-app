ef6e1bc072030c97f48b62f4e3713540f191a4e0
rod773 <104315743+rod773@users.noreply.github.com>
2024-02-11 20:45:22 +0100

    update

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { BsPlus } from "react-icons/bs";
 
 const Todo = () => {
+  const [newTodoText, setMewTodoText] = useState("");
+
   return (
     <div className="max-w-4xl mx-auto sm:mt-8 p-4 bg-gray-100 rounded">
       <h2 className="mb-3 mt-6 text-2xl font-bold text-center uppercase">
@@ -9,6 +11,8 @@ const Todo = () => {
       </h2>
       <div className="flex items-center mb-4">
         <input
+          value={newTodoText}
+          onChange={(e) => setMewTodoText(e.target.value)}
           type="text"
           name="text"
           id="addTodoInput"
