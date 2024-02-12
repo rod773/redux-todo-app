0c9e83d77d05f8bb6ee08be65988c7e66cbfe60d
rod7773 <rodrigoarcadio1@gmail.com>
2024-02-12 18:43:40 +0100

    todo list

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,3 +1,18 @@
+import { useSelector } from "react-redux";
+
 export default function TodoList() {
+  const filteredTodos = useSelector((state) => {
+    const todos = state.todos;
+    const filter = state.filter;
+    const searchItem = state.searchTerm;
+
+    return todos.filter((todo) => {
+      const matchFilter =
+        (filter === "COMPLETED" && todo.completed) ||
+        (filter === "INCOMPLETED" && !todo.completed) ||
+        filter === "ALL";
+    });
+  });
+
   return <div>TodoList</div>;
 }
