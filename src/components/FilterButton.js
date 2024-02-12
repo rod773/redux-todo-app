f9d29359e86cc62fd08cb61b2526afd68de267f5
rod7773 <rodrigoarcadio1@gmail.com>
2024-02-12 09:49:17 +0100

    update

--- src/components/FilterButton.js
+++ src/components/FilterButton.js
@@ -1,12 +1,23 @@
 import React from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { filterTodos } from "../redux/actions";
 
 const FilterButton = () => {
-  const dispatch = useDispatch;
+  const dispatch = useDispatch();
+
+  const currentFilter = useSelector((state) => state.filter);
+
+  const handleFilter = (filter) => {
+    dispatch(filterTodos(filter));
+  };
 
   return (
     <div className="flex space-x-4 items-center">
-      <select className="text-sm px-2 py-1 rounded border border-gray-300 focus:outline-none">
+      <select
+        value={currentFilter}
+        onChange={(e) => handleFilter(e.target.value)}
+        className="text-sm px-2 py-1 rounded border border-gray-300 focus:outline-none"
+      >
         <option value="ALL">Default</option>
         <option value="COMPLETED">Completed</option>
         <option value="INCOMPLETED">Incompleted</option>
