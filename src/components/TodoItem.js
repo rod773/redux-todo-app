3beae3daacc1e2fd92dd7f4edb130e396e700828
rod7773 <rodrigoarcadio1@gmail.com>
2024-02-12 19:46:46 +0100

    update

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -5,8 +5,15 @@ export default function TodoItem(todo, index) {
     <li className="flex flex-col sm:flex-row sm;items-center justify-between border-b-2 py-3 gap-4">
       <div className="flex items-center">
         <span className="mr-4 text-gray-500">{index + 1}</span>
-        <span>{todo.text}</span>
+        <span
+          className={`mr-4 ${
+            todo.completed ? "line-throught text-500-500" : ""
+          }`}
+        >
+          {todo.text}
+        </span>
       </div>
+      <div></div>
     </li>
   );
 }
